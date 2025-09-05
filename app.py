73e852bc11c60dc628c7bc1552afdcf1069e7ac3
ogbox <tiwarivaibhav3672@gmail.com>
2025-09-05 19:14:58 +0530

    renderrr issuess

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 model = genai.GenerativeModel(
-    model_name="gemini-1.0-pro",
+    model_name="gemini-2.5-pro",
     generation_config=generation_config,
     safety_settings=safety_settings
 )
